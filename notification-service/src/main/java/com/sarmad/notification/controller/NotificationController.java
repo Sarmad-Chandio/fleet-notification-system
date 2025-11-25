775150c6ec76f542d4f87df8d7a6b5d0355157d2
Sarmad <engrsarmadchandio@gmail.com>
2025-11-25 20:52:28 +0500

    adding entities class and adding config class

---
+++ notification-service/src/main/java/com/sarmad/notification/controller/NotificationController.java
@@ -0,0 +1,21 @@
+package com.sarmad.notification.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NotificationController {
+
+    /*
+    /topic/alerts
+
+    /topic/driver-status
+
+    /topic/vehicle-alerts/{id}
+
+    Add DB tables:
+    alerts
+    notification_status
+    alert_history
+*/
+
+}
