0af8246dd4ccebe0c90a7f6c841a7e727897f43b
Sarmad <engrsarmadchandio@gmail.com>
2025-12-06 15:58:45 +0500

    Websocket config change

--- notification-service/src/main/java/com/sarmad/notification/config/WebSocketConfig.java
+++ notification-service/src/main/java/com/sarmad/notification/config/WebSocketConfig.java
@@ -5,6 +5,10 @@ import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.messaging.converter.MessageConverter;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -21,5 +25,10 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.addEndpoint("/ws")
                 .setAllowedOrigins("*")
                 .withSockJS();
+    }
+    @Override
+    public boolean configureMessageConverters(List<MessageConverter> messageConverters) {
+        messageConverters.add(new MappingJackson2MessageConverter());
+        return false;
     }
 }
