40dc4ccd3448b6a7541f08890a7cd47b159871f0
Sarmad <engrsarmadchandio@gmail.com>
2025-12-13 11:40:44 +0500

    commiting all files

---
+++ tracking-service/src/test/java/com/sarmad/trackingservice/TrackingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sarmad.trackingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackingServiceApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
